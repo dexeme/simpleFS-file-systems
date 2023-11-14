cbbcac340f9c349af2ac62cfc7b365f8b38ef5a5
enzoniko <enzonsb@gmail.com>
2023-11-14 10:10:53 -0300

    implemented debug, need to fix and test format

--- fs.cc
+++ fs.cc
@@ -1,8 +1,42 @@
 #include "fs.h"
+#include <cmath>
 
 int INE5412_FS::fs_format()
 {
-	return 0;
+	// Se o disco já foi montado não pode formatar, retorna falha
+	// TODO: Avaliar se isso está bom, e pensar em outros casos de falha
+	if (disk->size() > 0) {
+		return 0;
+	}
+
+	union fs_block block;
+
+	block.super.magic = FS_MAGIC;
+	block.super.nblocks = disk->size();
+	// 10% dos blocos são reservados para inodes arredondado para cima 
+	block.super.ninodeblocks = ceil(block.super.nblocks * 0.1);
+	block.super.ninodes = block.super.ninodeblocks * INODES_PER_BLOCK;
+
+	// Escreve o superbloco
+	disk->write(0, block.data);
+
+	// Libera a tabela de inodos
+	for (int i = 1; i <= block.super.ninodeblocks; i++) {
+		for (int j = 0; j < INODES_PER_BLOCK; j++) {
+			block.inode[j].isvalid = 0;
+		}
+		disk->write(i, block.data);
+	}
+
+	// Nulifica os ponteiros dos blocos de dados
+	for (int i = block.super.ninodeblocks + 1; i < block.super.nblocks; i++) {
+		for (int j = 0; j < POINTERS_PER_BLOCK; j++) {
+			block.pointers[j] = 0;
+		}
+		disk->write(i, block.data);
+	}
+
+	return 1;
 }
 
 void INE5412_FS::fs_debug()
@@ -16,6 +50,36 @@ void INE5412_FS::fs_debug()
  	cout << "    " << block.super.nblocks << " blocks\n";
 	cout << "    " << block.super.ninodeblocks << " inode blocks\n";
 	cout << "    " << block.super.ninodes << " inodes\n";
+
+	for (int i = 1; i <= block.super.ninodeblocks; i++) {
+		disk->read(i, block.data);
+
+		for (int j = 0; j < INODES_PER_BLOCK; j++) {
+			if (block.inode[j].isvalid) {
+				cout << "inode " << j << ":\n";
+				cout << "    size: " << block.inode[j].size << " bytes\n";
+				cout << "    direct blocks:";
+				for (int k = 0; k < POINTERS_PER_INODE; k++) {
+					if (block.inode[j].direct[k] != 0) {
+						cout << " " << block.inode[j].direct[k];
+					}
+				}
+				cout << "\n";
+				cout << "    indirect block: " << block.inode[j].indirect << "\n";
+
+				if (block.inode[j].indirect != 0) {
+					disk->read(block.inode[j].indirect, block.data);
+					cout << "    indirect data blocks:";
+					for (int k = 0; k < POINTERS_PER_BLOCK; k++) {
+						if (block.pointers[k] != 0) {
+							cout << " " << block.pointers[k];
+						}
+					}
+					cout << "\n";
+				}
+			}
+		}
+	}
 }
 
 int INE5412_FS::fs_mount()
